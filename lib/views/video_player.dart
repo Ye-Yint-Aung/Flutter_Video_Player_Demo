8bee354efb2aa4ae1bcd9ad8cab07b728efaf258
YeYintAung <yeyintaung2@securelinkmm.com>
2023-10-09 14:35:05 +0630

    Slider and play/pause feature add

--- lib/views/video_player.dart
+++ lib/views/video_player.dart
@@ -12,14 +12,18 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   late VideoPlayerController controller;
 
   String videoUrl =
-      'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4';
+      //'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4';
+      'https://dsqqu7oxq6o1v.cloudfront.net/motion-array-1236368-Spaceship_01-high.mp4';
 
+  bool _isPlayBtnClick = true;
+  double _currentSliderSecondaryValue = 0.0;
 
   @override
   void initState() {
     super.initState();
     controller = VideoPlayerController.networkUrl(Uri.parse(videoUrl));
-
+    _currentSliderSecondaryValue =
+        controller.value.position.inMilliseconds.toDouble();
     controller.addListener(() {
       setState(() {});
     });
@@ -33,26 +37,106 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
     controller.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
         backgroundColor: Colors.blue,
-        title:const Text("API Video Player"),),
+        title: const Text("API Video Player"),
+      ),
       body: Center(
-        child: InkWell(
-          onTap: () {
-            if (controller.value.isPlaying) {
-              controller.pause();
-            } else {
-              controller.play();
-            }
-          },
-          child: AspectRatio(
-            aspectRatio: controller.value.aspectRatio,
-            child: VideoPlayer(controller),
-          ),
+        child: Column(
+          children: [
+            InkWell(
+              onTap: () {
+                if (controller.value.isPlaying) {
+                  controller.pause();
+                } else {
+                  controller.play();
+                }
+              },
+              child: AspectRatio(
+                aspectRatio: controller.value.aspectRatio,
+                child: VideoPlayer(controller),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                    "${controller.value.position.inHours}:${controller.value.position.inMinutes.remainder(60)}:${(controller.value.position.inSeconds.remainder(60))}"),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                    child: SliderTheme(
+                      data: SliderThemeData(
+                          activeTrackColor: Colors.blue,
+                          inactiveTrackColor: Colors.black,
+                          trackHeight: 3,
+                          overlayShape: SliderComponentShape.noThumb),
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Padding(
+                            padding:const EdgeInsets.symmetric(horizontal: 1),
+                            child: Slider(
+                              min: 0.0,
+                              max: controller.value.duration.inMilliseconds
+                                  .toDouble(),
+                              value: controller.value.position.inMilliseconds
+                                  .toDouble(),
+                              onChanged: (value) {
+                                setState(() {
+                                  controller.seekTo(
+                                      Duration(milliseconds: value.toInt()));
+                                });
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Text(
+                    "${controller.value.duration.inHours}:${controller.value.duration.inMinutes.remainder(60)}:${(controller.value.duration.inSeconds.remainder(60))}")
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                    onPressed: () {
+                      print("You clicked Previous Button");
+                    },
+                    icon: const Icon(Icons.skip_previous)),
+                IconButton(
+                    onPressed: () {
+                      //_isPlayBtnClick? true : false;
+                      if (_isPlayBtnClick == true) {
+                        setState(() {
+                          _isPlayBtnClick = false;
+                          controller.pause();
+                        });
+                      } else {
+                        setState(() {
+                          _isPlayBtnClick = true;
+                          controller.play();
+                        });
+                      }
+                    },
+                    icon: _isPlayBtnClick
+                        ? const Icon(Icons.play_arrow)
+                        : const Icon(Icons.pause)),
+                IconButton(
+                    onPressed: () {
+                      print("You clicked Next Button");
+                    },
+                    icon: const Icon(Icons.skip_next)),
+              ],
+            )
+          ],
         ),
       ),
     );
